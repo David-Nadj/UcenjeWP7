6c2efe04fa107d6429dffe6c07885730cd2dfbaf
David-Nadj <david08.nadj@gmail.com>
2024-11-27 19:20:20 +0100

    osnovne naredbe za dropako postoji i kreiranje baze

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,7 +1,12 @@
-﻿--create database edunovawp7;
+﻿use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7;
+go
+use edunovawp7;
+go
 
---use edunovawp7;
-
-create table smjer (
+create table smjer(
 naziv varchar(50)
 );
