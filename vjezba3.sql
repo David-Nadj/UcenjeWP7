b118bf4eebe99efd2e509a119f6b666aef6d4446
David-Nadj <david08.nadj@gmail.com>
2024-11-27 21:02:14 +0100

    vjezba vjezbe 3

---
+++ vjezba3.sql
@@ -0,0 +1,24 @@
+﻿use master;
+go
+drop database if exists vjezba3;
+go
+create database vjezba3;
+go
+use vjezba3;
+go
+
+create table IspitniRok (
+sifra int,
+predmet varchar(50),
+vrstaIspita varchar(50),
+datum varchar(50),
+pristupio
+);
+
+
+create table Pristupnici (
+ispitniRok int,
+student varchar(50)
+brojBodova varchar(100)
+ocjena varchar(60)
+);
