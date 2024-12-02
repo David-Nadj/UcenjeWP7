bdcbf2b7640820e0458a77cca8aaf3b68db11cee
David-Nadj <david08.nadj@gmail.com>
2024-12-02 20:23:02 +0100

    nedovršeni nogomet

--- nogomet.sql
+++ nogomet.sql
@@ -8,11 +8,43 @@ use nogomet;
 go
 
 create table klub (
-sifra int primary key identity(1,1)
-naziv varchar(50),
-osnovan datetime,
-stadion varchar(100),
-predsjednik varchar(50),
-drzava varchar(50),
-liga varchar (100)
+sifra int primary key identity(1,1),
+naziv varchar(50) not null,
+osnovan datetime not null,
+stadion varchar(100) not null,
+predsjednik varchar(50) not null,
+drzava varchar(50) not null,
+liga int not null
 );
+
+create table utakmica (
+sifra int not null references klub(sifra),
+datum datetime not null,
+vrijeme datetime not null,
+lokacija varchar(100) not null,
+stadion varchar(100) not null,
+domaci_klub varchar(50) not null,
+gostujuci_klub varchar(50) not null,
+);
+
+create table igrac(
+sifra int not null references klub(sifra),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+datum_rođenja datetime not null,
+pozicija varchar(50) not null,
+broj_dresa int not null,
+klub varchar(50) not null,
+);
+
+create table trener (
+sifra int not null references klub(sifra),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+klub varchar(100) not null ,
+nacionalnost varchar(100) not null,
+iskustvo varchar(100) not null,
+);
+
+
+
