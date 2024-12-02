3f9e5e57387f6222175f32a4a62f513608ac9547
David-Nadj <david08.nadj@gmail.com>
2024-12-02 20:12:57 +0100

    nogomet.sql

---
+++ nogomet.sql
@@ -0,0 +1,18 @@
+﻿use master;
+go
+drop database if exists nogomet;
+go
+create database nogomet;
+go
+use nogomet;
+go
+
+create table klub (
+sifra int primary key identity(1,1)
+naziv varchar(50),
+osnovan datetime,
+stadion varchar(100),
+predsjednik varchar(50),
+drzava varchar(50),
+liga varchar (100)
+);
