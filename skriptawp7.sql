85a76f009dc35a35e4fdfffe2901d2d8b7687874
David-Nadj <david08.nadj@gmail.com>
2025-03-07 16:59:30 +0100

    vježba ponavljanje

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,22 +1,24 @@
-﻿use master;
+﻿-- Ovako se pišu komentari
+
+use master;
 go
 drop database if exists edunovawp7;
 go
-create database edunovawp7;
+create database edunovawp7 collate  Croatian_CI_AS;
 go
 use edunovawp7;
 go
 
 create table smjerovi(
-sifra int not null primary key identity(1,1), -- primarni ključ
-naziv varchar(50)not null,
-trajanje int null, -- null se se nemora pisat
+sifra int not null primary key identity(1,1), -- ovo je primarni ključ
+naziv varchar(50) not null,
+trajanje int null, -- null se ne mora pisati
 cijena decimal(18,2),
 vaucer bit,
 izvodiseod datetime
 );
 
-create table polaznici (
+create table polaznici(
 sifra int not null primary key identity(1,1),
 ime varchar(50) not null,
 prezime varchar(50) not null,
@@ -25,11 +27,12 @@ email varchar(100) not null
 );
 
 create table grupe(
-sifra int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1), 
 naziv varchar(20) not null,
-smjer int not null references smjerovi(sifra), -- vanjski kljuc
+smjer int not null references smjerovi(sifra), -- ovo je vanjski ključ
 predavac varchar(50)
 );
+
 
 create table clanovi(
 grupa int not null references grupe(sifra),
---
+++ vjezba4.sql
@@ -0,0 +1,8 @@
+﻿use master;
+go
+drop database if exists vjezba4;
+go
+create database vjezba4;
+go
+use vjezba4;
+go
